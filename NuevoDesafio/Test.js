b6d131b64baa9e613dfcd2fa90a2c77b41b83292
MatiasAlderete <83623578+MatiasAlderete@users.noreply.github.com>
2021-09-22 20:19:59 -0300

    Add files via upload

--- NuevoDesafio/Test.js
+++ NuevoDesafio/Test.js
@@ -1,3 +1,22 @@
 const Contenedor = require('./Contenedor');
 
-const miContenedor = new Contenedor('./mascotas.json');
+const miContenedor = new Contenedor('./Productos.json');
+
+console.log(miContenedor);
+
+const misUtiles = {
+   utiles: 'Regla',
+   price: '345.40'
+};
+
+const main = async () => {
+    const id = await miContenedor.save(misUtiles);
+    console.log(id);
+
+//    const list = await miContenedor.getAll();
+//    console.log(list);
+
+//    await miContenedor.deleteAll();
+};
+
+main();
