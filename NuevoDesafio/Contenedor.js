f6be7e450a2a18300e02bba00edf1964bf4b6e67
MatiasAlderete <83623578+MatiasAlderete@users.noreply.github.com>
2021-09-21 21:27:36 -0300

    Add files via upload

--- NuevoDesafio/Contenedor.js
+++ NuevoDesafio/Contenedor.js
@@ -3,30 +3,50 @@ const fs = require('fs');
 class Contenedor {
     constructor(file) {
         this.file = file;
-        console.log('constructor: ', this);
     }
 
-    save(mascota) {
-        console.log('save: ', mascota);
+    async save(utiles) {
+     try {
+      const contenido = await fs.promises.readFile(`./${this.file}`, 'utf-8');
+        
+         let utiles = []; 
+         if (contenido === '') {
+            utiles.id = 1;
+            utiles.push(utiles);
+        } else {
+          const utiles = JSON.parse(contenido);
 
-        fs.promises.readFile(`./${this.file}` , '') {
-          .then((contenido) => {
-              console.log(contenido);
-              if (contenido === '') {
-                  mascota.id = 0;
-                  return mascota;
-              }
-              const listaDeMascota = JSON.parsel(contenido);
-              mascota.id = listaDeMascota.length;
-
-              return mascota;
-          })
-          .then (mascota) => {
-            const mascotaString = JSON.stringify(mascota, null, 2);
-            console.log('LISTA PARA GUARDAR: ', mascotaString);
-          }
-          
+          util.id = listaUtiles[listaUtiles.length -1].id +1;
+          listaUtiles.push(utiles);
+          utiles = listaUtiles
         }
-    } 
-}
 
+         const utilesString = JSON.stringify(utiles, null, 2);
+         await fs.promises.writeFile(`./${this.file}` , utilesString);
+         return util.id;
+      }  catch (error) {
+            console.error('error', error);
+        };
+    }
+
+/*     async getAll() {
+    try {
+          const contenido = await fs.promises.readFile(`./${this.file}`, 'utf-8');
+          const listaUtiles = JSON.parse(contenido);
+
+          return listaUtiles;
+    } catch (error) {
+          console.error('Error:', error);
+        };
+    }    
+
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(`./${this.file}`, '');    
+        } catch (error) {
+            console.error('Error:', error);
+        };
+    } */
+}        
+
+module.exports = Contenedor;
